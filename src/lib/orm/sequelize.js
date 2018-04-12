ab062c706052c2b0b209bcf75c23eca1f6e30a7a
Jeff Chung <chungchi300@hotmail.com>
2018-04-12 16:32:35 +0800

    modularization orm

--- src/lib/orm/sequelize.js
+++ src/lib/orm/sequelize.js
@@ -1,37 +1,47 @@
-const Sequelize = require('sequelize');
-const path = require('path');
-const fs = require('fs');
-const _ = require('lodash');
-const instanceMethods = require('./sequenlizeClassMethod.js');
-const config = smartRequire('config');
+const Sequelize = require('sequelize')
+const path = require('path')
+const fs = require('fs')
+const _ = require('lodash')
+const instanceMethods = require('./sequenlizeClassMethod.js')
+const config = smartRequire('config')
 var sequelize = new Sequelize(
   config.database.connection.database,
   config.database.connection.username,
   config.database.connection.password,
   config.database.extra
-);
+)
+function loadModel (modelsPath) {
+  const child = fs.readdirSync(modelsPath)
+  for (let model of child) {
+    let modelPath = path.resolve(modelsPath, model)
+    // console.log('the modal path', modelPath);
+    if (fs.statSync(modelPath).isFile()) {
+      let importedModel = sequelize.import(modelPath)
 
-let modelsPath = smartRequire('ormModelPath');
-const child = fs.readdirSync(modelsPath);
-for (let model of child) {
-  let modelPath = path.resolve(modelsPath, model);
-  // console.log('the modal path', modelPath);
-  if (fs.statSync(modelPath).isFile()) {
-    let importedModel = sequelize.import(modelPath);
-
-    for (let methodName of Object.keys(instanceMethods)) {
-      importedModel[methodName] = instanceMethods[methodName];
+      for (let methodName of Object.keys(instanceMethods)) {
+        importedModel[methodName] = instanceMethods[methodName]
+      }
     }
   }
+  let modelsByName = {}
+  Object.keys(sequelize.models).map(modelName => {
+    modelsByName[_.upperFirst(modelName)] = sequelize.models[modelName]
+  })
+  return modelsByName
 }
-let models = {};
-Object.keys(sequelize.models).map(modelName => {
-  models[_.upperFirst(modelName)] = sequelize.models[modelName];
-});
+let modelsPaths = smartRequire('ormModelPaths')
+var modelsByName = {}
+modelsPaths.forEach(modelsPath => {
+  modelsByName = Object.assign(modelsByName, loadModel(modelsPath))
+})
+
 module.exports = {
-  orm: models,
-  sequelize: sequelize,
-};
+  orm: modelsByName,
+  sequelize: sequelize
+}
 //
 
-require(smartRequire('ormRelationModelPath'));
+let relationPaths = smartRequire('ormRelationPaths')
+relationPaths.forEach(relationPath => {
+  require(relationPath)
+})
--- src/smartRequire.js
+++ src/smartRequire.js
@@ -3,32 +3,35 @@
   can do dependency injection here if u want
 
 */
-global.smartRequire = function(name) {
+global.smartRequire = function (name) {
   if (name == 'mail') {
     if (process.env.NODE_ENV == 'test') {
-      return smartRequire('lib/mail/default');
+      return smartRequire('lib/mail/default')
     } else {
-      return smartRequire('lib/mail/sparkpost');
+      return smartRequire('lib/mail/sparkpost')
     }
   }
 
-  if (name == 'ormModelPath') {
-    return __dirname + '/service/orm/models';
+  if (name == 'ormModelPaths') {
+    return [
+      __dirname + '/service/Membership/orm/models',
+      __dirname + '/service/Analytic/orm/models'
+    ]
   }
-  if (name == 'ormRelationModelPath') {
-    return __dirname + '/service/orm/relation';
+  if (name == 'ormRelationPaths') {
+    return [__dirname + '/service/Analytic/orm/relation']
   }
-  //find method to avoid orm ,sequelize,config load more then  once
+  // find method to avoid orm ,sequelize,config load more then  once
   /*
   Node js already cached and only load once
   http://www.infoq.com/cn/articles/nodejs-module-mechanism
   */
 
   if (name == 'orm') {
-    return smartRequire('lib/orm/sequelize').orm;
+    return smartRequire('lib/orm/sequelize').orm
   }
   if (name == 'sequelize') {
-    return smartRequire('lib/orm/sequelize').sequelize;
+    return smartRequire('lib/orm/sequelize').sequelize
   }
-  return require(__dirname + '/' + name);
-};
+  return require(__dirname + '/' + name)
+}
