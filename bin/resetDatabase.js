41f1a857e8e77489a18fb1b2cb9990397e4615e6
Astri Jeff Chung <jeffchung@astri.org>
2018-05-22 18:11:30 +0800

    fix test case

--- bin/resetDatabase.js
+++ bin/resetDatabase.js
@@ -1,28 +1,28 @@
-var path = require('path');
-require('../src/smartRequire');
-const sequelize = smartRequire('sequelize');
-const orm = smartRequire('orm');
+var path = require("path");
+require("../src/smartRequire");
+const sequelize = smartRequire("sequelize");
+const orm = smartRequire("orm");
 
 async function reloadDatabase() {
-  console.log('reloading');
+  console.log("reloading");
   await sequelize.sync({ force: true });
 
   await orm.Feedback.create({
-    content: 'safe start koa2 is easy to use',
+    content: "safe start koa2 is easy h to use"
   });
   await orm.Feedback.create({
-    content: 'safe start koa2 help me finish my work eariler',
+    content: "safe start koa2 help me finish my work eariler"
   });
   process.exit(0);
 }
 try {
-  console.log('dirname', __dirname);
-  global.srcRoot = __dirname.replace('bin', 'src');
+  console.log("dirname", __dirname);
+  // global.srcRoot = __dirname.replace('bin', 'src');
   // var config = require(path.join(__dirname, '../src/config/default.js'));
 
   //
 
-  reloadDatabase().then(res => console.log('reload done'));
+  reloadDatabase().then(res => console.log("reload done"));
 } catch (err) {
   throw new Error(err.message);
   process.exit(1);
--- src/smartRequire.js
+++ src/smartRequire.js
@@ -3,23 +3,26 @@
   can do dependency injection here if u want
 
 */
-global.smartRequire = function (name) {
-  if (name == 'mail') {
-    if (process.env.NODE_ENV == 'test') {
-      return smartRequire('lib/mail/default')
+global.smartRequire = function(name) {
+  if (name == "srcRoot") {
+    return __dirname;
+  }
+  if (name == "mail") {
+    if (process.env.NODE_ENV == "test") {
+      return smartRequire("lib/mail/default");
     } else {
-      return smartRequire('lib/mail/sparkpost')
+      return smartRequire("lib/mail/sparkpost");
     }
   }
 
-  if (name == 'ormModelPaths') {
+  if (name == "ormModelPaths") {
     return [
-      __dirname + '/service/Membership/orm/models',
-      __dirname + '/service/Analytic/orm/models'
-    ]
+      __dirname + "/service/Membership/orm/models",
+      __dirname + "/service/Analytic/orm/models"
+    ];
   }
-  if (name == 'ormRelationPaths') {
-    return [__dirname + '/service/Analytic/orm/relation']
+  if (name == "ormRelationPaths") {
+    return [__dirname + "/service/Analytic/orm/relation"];
   }
   // find method to avoid orm ,sequelize,config load more then  once
   /*
@@ -27,11 +30,11 @@ global.smartRequire = function (name) {
   http://www.infoq.com/cn/articles/nodejs-module-mechanism
   */
 
-  if (name == 'orm') {
-    return smartRequire('lib/orm/sequelize').orm
+  if (name == "orm") {
+    return smartRequire("lib/orm/sequelize").orm;
   }
-  if (name == 'sequelize') {
-    return smartRequire('lib/orm/sequelize').sequelize
+  if (name == "sequelize") {
+    return smartRequire("lib/orm/sequelize").sequelize;
   }
-  return require(__dirname + '/' + name)
-}
+  return require(__dirname + "/" + name);
+};
