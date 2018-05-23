3d659c8e6e47c124dca30cc97dea3ac06a776ac9
Jeff Chung <chungchi300@hotmail.com>
2018-05-23 18:31:36 +0800

    admin

--- public/admin/src/App.tsx
+++ public/admin/src/App.tsx
@@ -1,17 +1,32 @@
-import * as React from "react";
-import Count from "~/containers/Count";
-import Author from "~/containers/Author";
-import "App.css";
+import * as React from 'react'
+import Count from '~/containers/Count'
+import Author from '~/containers/Author'
+
+import 'App.css'
+
+import { Admin, Resource } from 'react-admin'
+import * as simpleRestClient from 'ra-data-simple-rest'
+console.log('simpleRestClient', simpleRestClient)
+import { PostList, PostEdit, PostCreate } from './post'
+
 class App extends React.Component<any> {
   render() {
     return (
       <div className="App">
+        <Admin dataProvider={simpleRestClient('http://localhost:3000')}>
+          <Resource
+            name="posts"
+            list={PostList}
+            edit={PostEdit}
+            create={PostCreate}
+          />
+        </Admin>
         <Count />
 
         <Author />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
